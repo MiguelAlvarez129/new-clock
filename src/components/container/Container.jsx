f0617d304fd83eb61b806aa79c860fb953bce5e0
Miguel Alvarez <31868740+MiguelAlvarez129@users.noreply.github.com>
2022-07-11 09:27:32 -0400

    Update Container.jsx

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -4,23 +4,24 @@ import ClockContainer  from '../clock-container/ClockContainer'
 import CenterContainer from '../center-container/CenterContainer'
 import Timer from '../timer/Timer'
 import StopWatch from '../stopwatch/StopWatch'
-import { AlertTitle, useDisclosure, Button, Alert } from '@chakra-ui/react'
-import { CustomAlert, DragSign,DragImg } from '../../shared/styles'
+import { AlertTitle, useDisclosure, Button, Alert, Flex, Center } from '@chakra-ui/react'
+import { DragImg } from '../../shared/styles'
 import { motion, AnimatePresence } from 'framer-motion';
 const Container = () => {
 
   const {
     isOpen: isVisible,
     onOpen,
-  } = useDisclosure({ defaultIsOpen: true })
+    onClose,
+  } = useDisclosure({ defaultIsOpen: false })
   
   const [show,setShow] = useState(false)
+  const [hide,setHide] = useState(false)
   const ref = useRef(null)
   const id = useRef(0)
   const location = useLocation()
   useEffect(() => {
     const observer = new IntersectionObserver(callback,{threshold:0.9})
-    console.log(ref,observer )
     observer.observe(ref.current)
     
   }, [location.pathname]);
@@ -31,13 +32,15 @@ const Container = () => {
       setShow(true)
       id.current = setTimeout(() => {
         setShow(false)
-      }, 6000);
+      }, 4000);
     } else {
       if (id.current){
         clearTimeout(id.current)
       }
       setShow(false)
-      if (location.pathname === '/') onOpen()
+      if (location.pathname === '/' && !hide) {
+        onOpen()
+      }
     }
   }
     
@@ -45,6 +48,10 @@ const Container = () => {
     ref.current.scrollIntoView()
   }
 
+  const close = () =>{
+    setHide(true)
+    onClose()
+  }
   return (
     <>
     <AnimatePresence style={{width:'100%'}}>
@@ -74,15 +81,35 @@ const Container = () => {
       </motion.div>}
     </AnimatePresence>
 
-    {/* { isVisible && <DragSign>
-      <b>
-        You can click and drag the clock to a different space
-      </b>
-      <DragImg />
-      <Button colorScheme='teal' variant='ghost' onClick={onClose}>
-        Understood!
-      </Button>
-    </DragSign> }  */}
+    <AnimatePresence style={{width:'100%'}}  >
+    { isVisible && !hide && 
+    <motion.div key="drag" 
+    initial={{ opacity: 0 , }}
+    animate={{ opacity: 1,}}
+    exit={{ opacity: 0 , }}>
+      <Center w="100%" position={"fixed"} zIndex={10}>
+        <Alert 
+        bg={'blackAlpha.600'}
+        color={'gray.100'}
+        borderRadius={'4px'}
+        w={'min(600px,70%)'}
+        h={'350px'}
+        zIndex={100}>
+          
+          <Flex direction={'column'} align={"center"} h={'100%'} w={'100%'}>
+          <b>
+            You can click and drag the clock to a different place
+          </b>
+          <DragImg />
+          <Button colorScheme='teal'  onClick={close}>
+            Understood!
+          </Button>
+          </Flex>
+        </Alert> 
+      </Center>
+    </motion.div> 
+    } 
+    </AnimatePresence>
     <Routes>
       <Route path="/" element={<ClockContainer refValue={ref}/>}/>
       <Route path="/stopwatch" element={<CenterContainer  refValue={ref} element={<StopWatch/>}/>}/>
